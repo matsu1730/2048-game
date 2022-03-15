0ad073a8875388d1acd3a456dbd5593acae1dd92
matsu1730 <alf.matsuoka2000@gmail.com>
2022-03-15 12:02:13 -0300

    Finishing tile logic on JS

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 import Grid from "./Grid.js"
+import Tile from "./Tile.js"
 
 const gameBoard = document.getElementById("gameBoard")
 const grid = new Grid(gameBoard)
+
+console.log(grid.randomEmptyCell())
+
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
